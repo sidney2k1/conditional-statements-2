ba2a19ebb92e381bd391871fca078e7f036db472
sidney2k1 <sidsingla02@gmail.com>
2024-08-22 17:21:52 +0530

    main2

---
+++ main2.py
@@ -0,0 +1,18 @@
+a=10
+b=20
+c=20
+print(a!=b)
+print(b!=c)
+a="python"
+b="coding"
+if a!=b:
+    print(a,"and", b,"are different")
+a=4
+b=5
+if (a==1)!=(b==5):
+    print("hello")
+a=int(input("enter a number:"))
+if a%2!=0:
+    print(a,"is an odd number")
+else:
+    print("it is an even number")
